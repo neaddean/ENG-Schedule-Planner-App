f469f820c432b69433e612939d018f89609a2622
Dean De Carli <ddecarli@bu.edu>
2013-12-04 16:36:04 +0000

    PYTHON PARSER COMPLETED*******

--- parser/python/parserv2.py
+++ parser/python/parserv2.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python2
-import urllib2, re
-#import xml.etree.ElementTree as ElementTree
-from xml.etree import ElementTree
+import urllib.request, re
+import xml.etree.ElementTree as ElementTree
+from xml.etree import ElementTree as ET
 from xml.dom import minidom
+import unicodedata
+
+links = []
+
+parser = ET.XMLParser(encoding="utf-8")
 
 def prettify(elem):
     """Return a pretty-printed XML string for the Element.
@@ -11,59 +16,84 @@ def prettify(elem):
     reparsed = minidom.parseString(rough_string)
     return reparsed.toprettyxml(indent="  ")
 
-
-links = []
 url_list = ["http://www.bu.edu/academics/eng/courses/electrical-computer-engineering/0/",
             "http://www.bu.edu/academics/eng/courses/mechanical-engineering/0/",
             "http://www.bu.edu/academics/eng/courses/biomedical-engineering/0/",
             "http://www.bu.edu/academics/eng/courses/engineering-core/0/"]
-            
+
+delta = {}
 for base_url in url_list:
     i = 0
     while 1:
-        delta = []
+        string = ""
+        delta.clear()
         base_url = base_url.replace(str(i), str(i+1))
         i = i + 1
-        #print base_url
         try:
-            res = urllib2.urlopen(base_url)
+            res = urllib.request.urlopen(base_url)
         except ValueError:
-            print "INVALD URL"
-        source = res.read().decode('utf-8').replace("\t","")
-        delta += re.findall(r'href=[\'"](.*)[\'"]><strong>(.*) (.*) (.*): (.*)</strong></a><br />\n.*<span>.*:(.*)</span><br />\n([^<]+)', source)
-        if delta == []:
+            print ("INVALD URL")
+        source = res.read().decode("utf-8").replace("\t","").replace("\n","").replace('&', '&amp;')
+        string = re.findall('<ul class\=\"course\-feed\">(.*?)</ul>?', source, re.S)
+        if string == "":
             break
-        links += delta
-
-
-root = ElementTree.Element("courses")
+        string = u"<root>" + string[0] + u"</root>"
+        #string = unicodedata.normalize('NFKD', string).encode('ascii','ignore')
+        #print(string)
+        root = ET.fromstring(string)
+        #rootdean = etree.fromstring(string)
+        for child in root:
+            courseCode = re.findall(r'([A-Z]{3}) ([A-Z]{2}) ([0-9]{3}?):(.*)',child[0][0].text)
+            delta["school"] = courseCode[0][0]
+            delta["dept"] = courseCode[0][1]
+            delta["cid"] = courseCode[0][2]
+            delta["course"] = courseCode[0][3]
+            if not child.find("span") == None:
+                delta["prereqs"] = re.findall(r'([A-Z]{3}) ([A-Z]{2}) ([0-9]{3}?)',
+                                          child.find("span").text)
+                if len(delta["prereqs"]) > 3:
+                    break
+            delta["link"] = "http://www.bu.edu" + child[0].attrib['href']
+            delta["description"] = child.findall("br")[-1].tail.lstrip()
+            if "2cr" in delta["description"].replace(" ",""):
+                delta["credits"] = "2"
+            else:
+                delta["credits"] = "4"
+            links.append(delta.copy())
+        if not delta:
+            break
+    break
+    
+root = ET.Element("courses")
 
 for i in links:
-    if (not ":" in " ".join(i[1:4])) and int(i[3]) < 600:
-        course = ElementTree.SubElement(root, "course")
-        course.set ("name", i[4])
-        school = ElementTree.SubElement(course, "school")
-        school.text = i[1]
-        dept   = ElementTree.SubElement(course, "dept")
-        dept.text   = i[2]
-        cid     = ElementTree.SubElement(course, "cid")
-        cid.text    = i[3]
-        description = ElementTree.SubElement(course, "description")
-        description.text = i[6].lstrip()
-        credit = ElementTree.SubElement(course, "credits")
-        if "4 credit" in i[6] or "4 cr" in i[6] or "4cr" in i[6]:
-            credit.text = "4"
-        elif "2 credit" in i[6] or "2 cr" in [6] or "2cr" in i[6]:
-            credit.text = "2"
-            print "okay"
-        else:
-            credit.text = "4"
+    if int(i["cid"]) < 600:
+        course = ET.SubElement(root, "course")
+        course.set ("name", i["course"])
+        school = ET.SubElement(course, "school")
+        school.text = i["school"]
+        dept   = ET.SubElement(course, "dept")
+        dept.text   = i["dept"]
+        cid     = ET.SubElement(course, "cid")
+        cid.text    = i["cid"]
+        description = ET.SubElement(course, "description")
+        description.text = i["description"]
+        link = ET.SubElement(course, "link")
+        link.text = i["link"]
+        credit = ET.SubElement(course, "credits")
+        credit.text = i["credits"]
+        if "prereqs" in i:
+            ger = ""
+            for req in i["prereqs"]:
+                ger += " " + "".join(map(str,req))
+            prereqs = ET.SubElement(course, "prereqs")
+            prereqs.text = ger.lstrip()
 
-
+            
 towrite =  prettify(root)
 xmlfile = open("courses.xml", "w")
-xmlfile.write(towrite.encode('utf-8'))
+xmlfile.write(towrite)
 xmlfile.close()
 
-print "done"
+print("done")
 #print towrite
