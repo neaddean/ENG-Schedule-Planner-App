467c5d29027005c88b4823743c58ada330706774
nrmich <nrmich@bu.edu>
2013-12-05 13:55:16 -0500

    adding main menu, package for menu classes

---
+++ Application/ENG-Schedule-Planner/src/com/example/eng_schedule_planner/menus/mainMenuActivity.java
@@ -0,0 +1,22 @@
+package com.example.eng_schedule_planner.menus;
+
+import android.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class mainMenuActivity extends Activity {
+	
+		
+		Button newPlanButton;
+		Button loadPlanButton;
+		Button settingsButton;
+		
+		@Override
+		protected void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			//setContentView(R.layout.main_menu);
+			
+			
+		}
+}
