323740f5250eeb7d1cab768a71be3b9805ebb475
Annie Lane <aelane@bu.edu>
2013-12-26 22:36:54 -0500

    Some comments

--- Application/ENG-Schedule-Planner/src/com/sunTANDproductions/eng_schedule_planner/courseModel/Course.java
+++ Application/ENG-Schedule-Planner/src/com/sunTANDproductions/eng_schedule_planner/courseModel/Course.java
@@ -2,17 +2,27 @@ package com.sunTANDproductions.eng_schedule_planner.courseModel;
 
 import java.util.ArrayList;
 
-public class Course {
+public class Course extends GenericCourse {
 	
 	public final static int CATEGORY_ENG_CORE = 0;
 	public final static int CATEGORY_NAT_SCI = 1;
 	public final static int CATEGORY_MATH = 2;
 	public final static int CATEGORY_GEN_ED = 3;
+	//Ruthie would like all of these to be the same color to be consistent with the course planning sheet
+	//Maybe we do something where the required courses are all blue, but different shades?
+	//I'm just concerned about how we'd differentiate colors...
+	//Do we had a functionality where the user can change the color for electives/non-required?
+	//Do we need to mark instances of courses as required in order to indicate that it shouldn't be deleted?
 	public final static int CATEGORY_ME_REQ = 4;
 	public final static int CATEGORY_ECE_REQ = 5;
 	public final static int CATEGORY_BME_REQ = 6;
+	
+	
 	public final static int CATEGORY_DEFAULT = 7;
 	public final static int CATEGORY_GENERIC = 8;
+	
+	//Maybe we should add another color for the Electives while they are a generic course and not an actual course yet
+
 	
 	public Course(String name, String school, String dept, String cid,
 			String description, ArrayList<String> prereqs, int credits) {
